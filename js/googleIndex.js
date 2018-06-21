dc650798a3d90d51ae796a93a8dec13c30c3a8ad
Browsers <38656592+nhNet@users.noreply.github.com>
2018-06-21 18:50:02 +0200

    Update googleIndex.js

--- js/googleIndex.js
+++ js/googleIndex.js
@@ -47,12 +47,12 @@ function verifyMyUser(googleUser) {
                                        '</div>';
                 setTimeout(function(){window.location='https://nhnet.github.io/pro/';},2000);
          }else{
-            document.querySelector('.myInputPassword').value="&nbsp;<span style='color:red;'>Your password was incorrect!</span>";
+            document.querySelector('.myInputPassword').innerHTML="&nbsp;<span style='color:red;'>Your password was incorrect!</span>";
          }
        }else{
-           document.querySelector('.myInputEmail').value="&nbsp;<span style='color:red;'>Your email was incorrect!</span>";
+           document.querySelector('.myInputEmail').innerHTML="&nbsp;<span style='color:red;'>Your email was incorrect!</span>";
            if(inputPassword.length<8 || inputPassword.length>16){
-               document.querySelector('.myInputPassword').value="&nbsp;<span style='color:red;'>Your password was incorrect!</span>";
+               document.querySelector('.myInputPassword').innerHTML="&nbsp;<span style='color:red;'>Your password was incorrect!</span>";
          }
        }
    }
