add384488799a2a1465b05e3b104b8a9c82cf3db
Browsers <38656592+nhNet@users.noreply.github.com>
2018-06-21 18:39:09 +0200

    Update googleIndex.js

--- js/googleIndex.js
+++ js/googleIndex.js
@@ -33,20 +33,27 @@ function verifyMyUser(googleUser) {
    document.querySelector('.submit').onclick= function(){
       var inputEmail = document.querySelector('.myInputEmail').value;
       var inputPassword = document.querySelector('.myInputPassword').value;
-       if(inputEmail == profileEmail && inputPassword.length>=8){
-           document.body.innerHTML='<div style="height:64.9vh;" class="borderBox">'+
-                                     '<div class="loginBox"><br><br>'+
-                                     '<h1 style="color:#555555;font-size:50px;">Matching Data...</h1><br><br>'+
-                                     '<h1 style="color:#555555;font-size:30px;">Fullname: <br></h1><span style="color:#555555;font-size:20px;margin-left:0%;">'+profileName+'</span><br><br>'+
-                                     '<h1 style="color:#555555;font-size:30px;">Email: <br></h1><span style="color:#555555;font-size:20px;margin-left:0%;">'+inputEmail+'</span><br><br>'+
-                                     '<h1 style="color:#555555;font-size:30px;">Password: <br></h1><span style="color:#555555;font-size:20px;margin-left:0%;">'+inputPassword+'</span><br><br><br><br><br>'+
-                                     '<h1 style="color:#555555;text-align:right;font-size:30px;">All correct!</h1>'+
-                                     '<h1 style="color:#555555;text-align:right;font-size:40px;">Loading Content...</h1>'+
-                                     '</div>'+
-                                 '</div>';
-          setTimeout(function(){window.location='https://nhnet.github.io/pro/';},2000);
+       if(inputEmail == profileEmail){
+          if(inputPassword.length>=8 && inputPassword.length<=16){
+               document.body.innerHTML='<div style="height:64.9vh;" class="borderBox">'+
+                                           '<div class="loginBox"><br><br>'+
+                                           '<h1 style="color:#555555;font-size:50px;">Matching Data...</h1><br><br>'+
+                                           '<h1 style="color:#555555;font-size:30px;">Fullname: <br></h1><span style="color:#555555;font-size:20px;margin-left:0%;">'+profileName+'</span><br><br>'+
+                                           '<h1 style="color:#555555;font-size:30px;">Email: <br></h1><span style="color:#555555;font-size:20px;margin-left:0%;">'+inputEmail+'</span><br><br>'+
+                                           '<h1 style="color:#555555;font-size:30px;">Password: <br></h1><span style="color:#555555;font-size:20px;margin-left:0%;">'+inputPassword+'</span><br><br><br><br><br>'+
+                                           '<h1 style="color:#555555;text-align:right;font-size:30px;">All correct!</h1>'+
+                                           '<h1 style="color:#555555;text-align:right;font-size:40px;">Loading Content...</h1>'+
+                                           '</div>'+
+                                       '</div>';
+                setTimeout(function(){window.location='https://nhnet.github.io/pro/';},2000);
+         }else{
+            document.querySelector('#passwordLabel').innerHTML="<span style='color:red;'>Your password was incorrect!</span>";
+         }
        }else{
-           document.body.innerHTML="Oops";
+           document.querySelector('#emailLabel').innerHTML="<span style='color:red;'>Your email was incorrect!</span>";
+           if(inputPassword.length<8 || inputPassword.length>16){
+               document.querySelector('#passwordLabel').innerHTML="<span style='color:red;'>Your password was incorrect!</span>";
+         }
        }
    }
 }
