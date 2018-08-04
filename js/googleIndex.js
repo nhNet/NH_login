1603948dd24c74c01484ca4a4bb1535d268d8ad1
Browsers <38656592+nhNet@users.noreply.github.com>
2018-08-04 08:21:52 +0200

    Update googleIndex.js

--- js/googleIndex.js
+++ js/googleIndex.js
@@ -50,8 +50,19 @@ inputs2.addEventListener("keyup", function(event) {
   }
 });
 
-function Next(){
-    var inputEmail = document.querySelector('.myInputEmail').value;
+// Here is what happens when Google Signs u in correctly.
+function verifyMyUser(googleUser) {
+    // Refer to Google using this variable. This is the base for the useful variables for code users.
+    var profile = googleUser.getBasicProfile();
+    // Name  Google User Variables for future use!
+    var profileImg = profile.getImageUrl();
+    var profileFullName = profile.getName();
+    var profileEmail = profile.getEmail();
+    var names = profileFullName.split(" ");
+    var firstName = names[0];
+
+    document.querySelector('.submit').onclick = function() {
+        var inputEmail = document.querySelector('.myInputEmail').value;
         var inputPassword = document.querySelector('.myInputPassword').value;
         
         
@@ -148,21 +159,6 @@ function Next(){
             document.querySelector('.myInputPassword').value = '';
             document.querySelector('.myInputPassword').placeholder = "Your password was incorrect!";
         }
-}
-
-// Here is what happens when Google Signs u in correctly.
-function verifyMyUser(googleUser) {
-    // Refer to Google using this variable. This is the base for the useful variables for code users.
-    var profile = googleUser.getBasicProfile();
-    // Name  Google User Variables for future use!
-    var profileImg = profile.getImageUrl();
-    var profileFullName = profile.getName();
-    var profileEmail = profile.getEmail();
-    var names = profileFullName.split(" ");
-    var firstName = names[0];
-
-    document.querySelector('.submit').onclick = function() {
-        Next();
     }
 }
 
