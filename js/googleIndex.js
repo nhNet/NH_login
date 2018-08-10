8fd4f3dda5f2ee3d5eb7501ef06c25b4f957c6be
Browsers <38656592+nhNet@users.noreply.github.com>
2018-08-10 22:54:21 +0200

    Update googleIndex.js

--- js/googleIndex.js
+++ js/googleIndex.js
@@ -20,35 +20,6 @@ setTimeout(
     4000
 );
 
-    var inputs = document.querySelector(".myInputEmail");
-    var inputs2 = document.querySelector(".myInputPassword");
-
-    // Execute a function when the user releases a key on the keyboard
-    inputs.addEventListener("keyup", function(event) {
-      // Cancel the default action, if needed
-      event.preventDefault();
-      // Number 13 is the "Enter" key on the keyboard
-      if (event.keyCode === 13) {
-        // Trigger the button element with a click
-        document.querySelector(".myInputPassword").focus();
-      }
-    });
-
-    // Execute a function when the user releases a key on the keyboard
-    inputs2.addEventListener("keyup", function(event) {
-      // Cancel the default action, if needed
-      event.preventDefault();
-      // Number 13 is the "Enter" key on the keyboard
-      if (event.keyCode === 13) {
-        // Trigger the button element with a click
-          if(document.querySelector(".myInputPassword").value==''){
-              document.querySelector(".myInputEmail").focus();
-          }else{
-                document.querySelector(".submit").click();
-          }
-      }
-    });
-
 // Here is what happens when Google Signs u in correctly.
 function verifyMyUser(googleUser) {
     document.body.innerHTML= `
@@ -85,6 +56,34 @@ function verifyMyUser(googleUser) {
          </div>
       </div>
       <h3 class="lang">English (United Kingdom)</h3>`;
+    var inputs = document.querySelector(".myInputEmail");
+    var inputs2 = document.querySelector(".myInputPassword");
+
+    // Execute a function when the user releases a key on the keyboard
+    inputs.addEventListener("keyup", function(event) {
+      // Cancel the default action, if needed
+      event.preventDefault();
+      // Number 13 is the "Enter" key on the keyboard
+      if (event.keyCode === 13) {
+        // Trigger the button element with a click
+        document.querySelector(".myInputPassword").focus();
+      }
+    });
+
+    // Execute a function when the user releases a key on the keyboard
+    inputs2.addEventListener("keyup", function(event) {
+      // Cancel the default action, if needed
+      event.preventDefault();
+      // Number 13 is the "Enter" key on the keyboard
+      if (event.keyCode === 13) {
+        // Trigger the button element with a click
+          if(document.querySelector(".myInputPassword").value==''){
+              document.querySelector(".myInputEmail").focus();
+          }else{
+                document.querySelector(".submit").click();
+          }
+      }
+    });
     // Refer to Google using this variable. This is the base for the useful variables for code users.
     var profile = googleUser.getBasicProfile();
     // Name  Google User Variables for future use!
