f35c5143e1576cb440a3a98e510517de17200b20
Browsers <38656592+nhNet@users.noreply.github.com>
2018-08-26 11:33:42 +0200

    Update googleIndex.js

--- js/googleIndex.js
+++ js/googleIndex.js
@@ -7,7 +7,7 @@ loadScript("https://apis.google.com/js/platform.js");
 loadScript("https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js");
 loadScript("https://nhnet.github.io/overrideShutdownCode/overrideCode.js");
 
-document.body.innerHTML='<div style="opacity:0;margin-top:-500px;" class="g-signin2" data-onsuccess="verifyMyUser" data-onfailure="onFail"></div><img style="margin-left:'+window.innerWidth/2-40+'px;" align="center" width="80px" src="img/load.gif"><h3 class="lang">English (United Kingdom)</h3>';
+document.body.innerHTML='<div style="opacity:0;margin-top:-500px;" class="g-signin2" data-onsuccess="verifyMyUser" data-onfailure="onFail"></div><img style="margin-left:643px;" align="center" width="80px" src="img/load.gif"><h3 class="lang">English (United Kingdom)</h3>';
          setTimeout(function(){
             document.body.innerHTML='<div class="borderBox"><div class="loginBox"><h1 class="oops" style="font-size:50px;"><br>Unfortunately, you have got to signup again!<br><br><br>Creating a button...<h1></div></div>';
             setTimeout(function(){document.querySelector('.oops').innerHTML='5';},3000); 
